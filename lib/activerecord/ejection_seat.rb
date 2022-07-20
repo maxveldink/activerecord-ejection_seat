42a335134198c61e95ff60b427ce10cc8f848377
Max VelDink <max@audaciousaardvark.com>
2022-07-20 07:24:33 -0400

    Initial commit

---
+++ lib/activerecord/ejection_seat.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ejection_seat/version"
+
+module Activerecord
+  module EjectionSeat
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/activerecord/ejection_seat_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activerecord::EjectionSeat do
+  it "has a version number" do
+    expect(Activerecord::EjectionSeat::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
